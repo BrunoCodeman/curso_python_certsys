cd49e3a70fca177916d489c15c8ef6582f1e78fd
Bruno Codeman <brunobemfica@gmail.com>
2019-04-09 00:56:09 -0300

    -melhorando resolução

--- src/scripting.py
+++ src/scripting.py
@@ -59,13 +59,13 @@ def get_weather_from_city(city, country):
     gets weather from the given city of a specific country
     and saves on a file called 'weather_content.txt'
     """
+    key = __CONFIG__["weather_api_key"]
+    base_url = __CONFIG__["base_url"]
+    url = f"{base_url}/data/2.5/weather?q={city},{country}&APPID={key}"
+    
     with open(f"{__path__}weather_content.json","w") as f:
-        key = __CONFIG__["weather_api_key"]
-        base_url = __CONFIG__["base_url"]
-        url = f"{base_url}/data/2.5/weather?q={city},{country}&APPID={key}"
         res = requests.get(url)
         f.write(res.text)
-
 
 if __name__ == "__main__":
     try:
