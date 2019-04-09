10bd49a376bea72c809b5d541c845defa5a075bc
Bruno Codeman <brunobemfica@gmail.com>
2019-04-09 00:53:46 -0300

    - adicionando scripting

--- src/scripting.py
+++ src/scripting.py
@@ -1,40 +1,42 @@
-#algo usando requests
-#leitura de parâmetros através de sys.argv no ifmain
 import os
 import sys
 import json
-import subprocess
 import psutil
 import requests
 
-log_file_path = "/var/log/apt/history.log"
 
-__CONFIG__ = json.load(open("configs.json","r"))
+#salve seus arquivos neste diretório 
+__path__ = f"{diretorio_do_projeto}/src/"
+__CONFIG__ = json.load(open(f"{__path__}configs.json","r"))
+apt_log_file_path = "/var/log/apt/history.log"
+
 
 def get_hardware_use():
     """
     Shows python's hardware consumption and total memory/cpu usage
     """
-    pass
+    
+    info = f""" 
+                python cpu usage: {python_cpu_usage} %
+                cpu usage: {cpu_usage_in_percentage} %
+                used memory: {used_memory_in_gb} GB
+                available memory: {available_memory_in_gb} GB
+            """
+    return info
 
 
 def json_apt_history_info():
     """
-    Generates a json file with apt history
+    Generates a json file with apt history log. It is saved as json_log.json
     """
     pass
-
-
+            
+            
+                
 
 def get_weather_from_city(city, country):
     """
-    Shows weather from given city from selected country
+    gets weather from the given city of a specific country
+    and saves on a file called 'weather_content.txt'
     """
-
     pass
-
-
-if __name__ == "__main__":
-    pass
-    
-
