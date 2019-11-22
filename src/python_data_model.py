b08f63bc05a002ef1913bc37d9b0bc6e1962e9b7
bruno <bruno.bemfica@certsys.com.br>
2019-11-22 16:42:14 -0300

    exercícios concluídos

--- src/python_data_model.py
+++ src/python_data_model.py
@@ -1,9 +1,23 @@
+import datetime
 class MultiplyList:
-    def __init__(self, max_size=0, multiplier=1):
-        pass
+    """Class to implement an iterator
+    of numbers multiplied"""
 
+    def __init__(self, max = 0, mult=1):
+        pass
     def __iter__(self):
         pass
 
     def __next__(self):
+        pass
+
+class Person:
+
+    def __init__(self, birth_date, name):
+        pass
+
+    def age(self):
+        pass
+
+    def __call__(self):
         pass
--- tests/test_python_data_model.py
+++ tests/test_python_data_model.py
@@ -1,30 +1,37 @@
 import unittest
+import datetime
 from src.python_data_model import *
 
 
 class PythonDataModelTest(unittest.TestCase):
     
-
     def test_multiply_list_must_be_iterable(self):
+        size = 5
         multiplier = 4
-        ml = MultiplyList(5, multiplier)
+        expecteds = [4,8,12,16,20]
+        ml = MultiplyList(size, multiplier)
         _it = iter(ml)
-        self.assertEqual(next(_it), 4)
-        self.assertEqual(next(_it), 8)
-        self.assertEqual(next(_it), 12)
-        self.assertEqual(next(_it), 16)
-        self.assertEqual(next(_it), 20)
+
+        for i in expecteds:
+            self.assertEqual(next(_it), i)
     
     def test_multiply_list_must_run_over_for_loop(self):
-        multiplier = 4
-        ml = MultiplyList(5, multiplier)
+        size = 8
+        multiplier = 10
+        expecteds = [10, 20, 30, 40, 50, 60, 70, 80]
+        ml = MultiplyList(size, multiplier)
         
         for i in ml:
-            self.assertEqual(i, )
+            self.assertIn(i, expecteds)
         
 
-    def test_account_must_have_balance_property(self):
-        pass
+    def test_person_class_must_have_age_property(self):
+        birth_date, name = datetime.datetime(1985,12,14), "Bruno"
+        _person = Person(birth_date, name)
+        self.assertEqual(_person.age, 33)
 
     def test_account_must_be_callable(self):
-        pass
+        birth_date, name = datetime.datetime(1985,12,14), "Bruno"
+        _person = Person(birth_date, name)
+        _expected = f"{name} has {_person.age} years"
+        self.assertEqual(_person(), _expected)
